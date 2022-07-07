2d9a5b8eb78ff23a57ea31fa074dd46665d85889
y3ff18 <2314787352@qq.com>
2022-07-07 22:12:40 +0800

    from_xlsx_to_target

--- from_xlsx_to_target.py
+++ from_xlsx_to_target.py
@@ -1,17 +1,22 @@
 import pandas
 import sys
 import os
+import argparse
 
-def read_file(file):    
+arg = argparse.ArgumentParser()
+arg.add_argument("-p","--path",help="Set Start Path")
+arg.add_argument("-t","--target",help="Set Find Target")
+parse = arg.parse_args()
+def read_file(file):
     print(f"读取 {file} 至内存中...")
     pd = pandas.read_excel(file,sheet_name="Sheet1")
     for i in pd.index:
         # print(pd.loc[i].values[6])
-        if "33252519990618" in pd.loc[i].values[6]:
+        if str(parse.target) in pd.loc[i]:
             print(f"找到了 {file}")
             sys.exit()
 
 
-for paths,dirs,files in os.walk(r"C:\Users\Magic\Desktop\户籍数据1.03GB公安专用"):
+for paths,dirs,files in os.walk(parse.path):
     for file in files:
         read_file(file=file)
